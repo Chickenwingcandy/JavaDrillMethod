69abb34a3e4f81424dccb5f1e564d49b876ebf99
Buccatta <chickenwingcandy23@gmail.com>
2021-09-17 14:38:24 -0400

    First push with the first 7 drills

---
+++ src/main/java/MethodDrills.java
@@ -0,0 +1,37 @@
+public class MethodDrills {
+
+    private int numberArray [] = {2,3,4};
+
+    public String giveString(){
+        return "Please don't give the answer. ";
+    }
+
+    private int giveNumber(){
+        return 5;
+    }
+
+    public int[] getNumberArray(){
+        return numberArray;
+    }
+
+    public void nothing (){
+
+    }
+
+    public String sentence(){
+        return "This is just to write a public method";
+    }
+
+    private String followSentence(){
+        return "This is just to write a private method";
+    }
+
+    public boolean trueOrfalse(){
+        return true;
+    }
+
+    
+
+
+
+}
